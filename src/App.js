088dd2217dfa920537f1a79d483113399b99220d
sampath surineni <ssurineni@salesforce.com>
2023-06-23 20:41:32 +0530

    commented other code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react'
 import './App.css';
+import { useState } from 'react';
 
+function Row(props) {
+  const nameForm = useRef(null)
+
+  const handleClickEvent = () => {
+    const form = nameForm.current
+    const key = form['key'].value
+    const value = form['value'].value
+    console.log('ironman key, value', key, value);
+    props.onClick(form['key'].value, form['value'].value)
+  }
+  return (<div>
+    <form ref={nameForm}>
+      <input label={'key'} name={'key'} />
+      <input label={'value'} name={'value'} />
+    </form><button onClick={handleClickEvent}>Save</button></div>)
+}
 function App() {
+  const [variables, setVariables] = useState(new Map());
+  const addVariable = (key, value) => setVariables(new Map(variables.set(key, value)));
+  const nameForm = useRef(null)
+
+  const handleClickEvent = () => {
+    const form = nameForm.current
+    const key = form['key'].value
+    const value = form['value'].value
+    console.log('ironman key, value', key, value);
+    addVariable(form['key'].value, form['value'].value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form ref={nameForm}>
+          <input label={'key'} name={'key'} />
+          <input label={'value'} name={'value'} />
+        </form><button onClick={handleClickEvent}>Save</button></div>
+      {/* {JSON.stringify(variables)} */}
+      {/* <div><Row onClick={addVariable} /></div> */}
+      {/* <ul>
+        {[...variables.keys()].map(k => (
+          <li key={k}>{variables.get(k)}</li>
+        ))}
+      </ul> */}
+      <ul>
+        {[...variables.keys()].map(key => (
+          <li key={key}>
+            <input label={key} name={key} value={key} />
+            <input label={variables.get(key)} name={variables.get(key)} value={variables.get(key)} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
